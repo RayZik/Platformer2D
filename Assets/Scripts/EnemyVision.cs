3c47b1044aaac4dea370027e94d9f22b0d975652
rayzik <a.zikrackiy@gmail.com>
2023-12-27 21:01:24 +0600

    Add EnemyVision to hit player

--- Assets/Scripts/EnemyVision.cs
+++ Assets/Scripts/EnemyVision.cs
@@ -4,8 +4,43 @@ using UnityEngine;
 
 public class EnemyVision : MonoBehaviour
 {
-    [SerializeField] private GameObject currentGameObject;
-    [SerializeField] private float circleRadius;
-    [SerializeField] private float maxDistance;
+    [SerializeField] private GameObject currentHitObject;
+    [SerializeField] private float circleRadius = 0.5f;
+    [SerializeField] private float maxDistance = 3;
     [SerializeField] private LayerMask layerMask;
+
+    private Vector2 _origin;
+    private Vector2 _direction;
+    private float _currentHitDistance;
+
+    private void Update()
+    {
+        _origin = transform.position;
+        _direction = Vector2.right;
+
+        RaycastHit2D hit = Physics2D.CircleCast(_origin, _currentHitDistance, _direction, maxDistance, layerMask);
+
+        if (hit)
+        {
+            currentHitObject = hit.transform.gameObject;
+            _currentHitDistance = hit.distance;
+
+            if (currentHitObject.CompareTag("Player"))
+            {
+
+            }
+        }
+        else
+        {
+            currentHitObject = null;
+            _currentHitDistance = maxDistance;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(_origin, _origin + _direction * _currentHitDistance);
+        Gizmos.DrawWireSphere(_origin + _direction * _currentHitDistance, circleRadius);
+    }
 }
